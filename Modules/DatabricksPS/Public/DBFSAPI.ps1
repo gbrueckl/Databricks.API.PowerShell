bbdc42fb6266dbcfa8cc298c6de4e6327f36d228
Gerhard Brueckl <gerhard@gbrueckl.at>
2018-11-09 09:10:13 +0100

    extended DBFSAPI

--- Modules/DatabricksPS/Public/DBFSAPI.ps1
+++ Modules/DatabricksPS/Public/DBFSAPI.ps1
@@ -101,7 +101,7 @@ Function Close-FSFile
 			.PARAMETER Handle 
 			The handle on an open stream. This field is required.
 			.EXAMPLE
-			Close-FSFile -Handle <handle>
+			Close-FSFile -Handle 7904256
 	#>
 	[CmdletBinding()]
 	param
@@ -123,6 +123,144 @@ Function Close-FSFile
 	#Set parameters
 	$parameters = @{
 		handle = $Handle 
+	}
+			
+	$parameters = $parameters | ConvertTo-Json
+
+	$result = Invoke-RestMethod -Uri $apiUrl -Method $requestMethod -Headers $headers -Body $parameters
+
+	return $result
+}
+
+Function Remove-FSFile
+{
+	<#
+			.SYNOPSIS
+			Delete the file or directory (optionally recursively delete all files in the directory). This call will throw an exception with IO_ERROR if the path is a non-empty directory and recursive is set to false or on other similar errors.
+			.DESCRIPTION
+			Delete the file or directory (optionally recursively delete all files in the directory). This call will throw an exception with IO_ERROR if the path is a non-empty directory and recursive is set to false or on other similar errors.
+			Official API Documentation: https://docs.databricks.com/api/latest/dbfs.html#delete
+			.PARAMETER Path 
+			The path of the file or directory to delete. The path should be the absolute DBFS path (e.g. "/mnt/foo/"). This field is required.
+			.PARAMETER Recursive 
+			Whether or not to recursively delete the directory's contents. Deleting empty directories can be done without providing the recursive flag.
+			.EXAMPLE
+			Remove-FSFile -Path "/MyFolder" -Recursive $false
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true, Position = 1)] [string] $Path, 
+		[Parameter(Mandatory = $false, Position = 2)] [bool] $Recursive = $false
+	)
+
+	Test-Initialized
+
+	Write-Verbose "Setting final ApiURL ..."
+	$apiUrl = Get-ApiUrl -ApiEndpoint "/2.0/dbfs/delete"
+	$requestMethod = "POST"
+	Write-Verbose "API Call: $requestMethod $apiUrl"
+
+	#Set headers
+	$headers = Get-RequestHeader
+
+	Write-Verbose "Setting Parameters for API call ..."
+	#Set parameters
+	$parameters = @{
+		path = $Path 
+		recursive = $Recursive 
+	}
+			
+	$parameters = $parameters | ConvertTo-Json
+
+	$result = Invoke-RestMethod -Uri $apiUrl -Method $requestMethod -Headers $headers -Body $parameters
+
+	return $result
+}
+
+Function Get-FSItem
+{
+	<#
+			.SYNOPSIS
+			Gets the file information of a file or directory. If the file or directory does not exist, this call will throw an exception with RESOURCE_DOES_NOT_EXIST.
+			.DESCRIPTION
+			Gets the file information of a file or directory. If the file or directory does not exist, this call will throw an exception with RESOURCE_DOES_NOT_EXIST.
+			Official API Documentation: https://docs.databricks.com/api/latest/dbfs.html#get-status
+			.PARAMETER Path 
+			The path of the file or directory. The path should be the absolute DBFS path (e.g. "/mnt/foo/"). This field is required.
+			.PARAMETER ChildItems 
+			Defines whether information of the item or its child items are returned. This field is not required. Default is 'false'.
+			.EXAMPLE
+			Get-DatabricksFSItem -Path "/myFolder/myFile"
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true, Position = 1)] [string] $Path,
+		[Parameter(Mandatory = $false, Position = 2)] [bool] $ChildItems = $false
+	)
+
+	Test-Initialized
+
+	Write-Verbose "Setting final ApiURL ..."
+	if($ChildItems)
+	{
+		$apiUrl = Get-ApiUrl -ApiEndpoint "/2.0/dbfs/list"
+	}
+	else
+	{
+		$apiUrl = Get-ApiUrl -ApiEndpoint "/2.0/dbfs/get-status"
+	}
+	$requestMethod = "GET"
+	Write-Verbose "API Call: $requestMethod $apiUrl"
+
+	#Set headers
+	$headers = Get-RequestHeader
+
+	Write-Verbose "Setting Parameters for API call ..."
+	#Set parameters
+	$parameters = @{
+		path = $Path 
+	}
+			
+	$result = Invoke-RestMethod -Uri $apiUrl -Method $requestMethod -Headers $headers -Body $parameters
+
+	return $result
+}
+
+Function Add-FSDirectory
+{
+	<#
+			.SYNOPSIS
+			Creates the given directory and necessary parent directories if they do not exist. If there exists a file (not a directory) at any prefix of the input path, this call will throw an exception with RESOURCE_ALREADY_EXISTS. Note that if this operation fails it may have succeeded in creating some of the necessary parent directories.
+			.DESCRIPTION
+			Creates the given directory and necessary parent directories if they do not exist. If there exists a file (not a directory) at any prefix of the input path, this call will throw an exception with RESOURCE_ALREADY_EXISTS. Note that if this operation fails it may have succeeded in creating some of the necessary parent directories.
+			Official API Documentation: https://docs.databricks.com/api/latest/dbfs.html#mkdirs
+			.PARAMETER Path 
+			The path of the new directory. The path should be the absolute DBFS path (e.g. "/mnt/foo/"). This field is required.
+			.EXAMPLE
+			Add-FSDirectory -Path "/myNewFolder"
+	#>
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true, Position = 1)] [string] $Path
+	)
+
+	Test-Initialized
+
+	Write-Verbose "Setting final ApiURL ..."
+	$apiUrl = Get-ApiUrl -ApiEndpoint "/2.0/dbfs/mkdirs"
+	$requestMethod = "POST"
+	Write-Verbose "API Call: $requestMethod $apiUrl"
+
+	#Set headers
+	$headers = Get-RequestHeader
+
+	Write-Verbose "Setting Parameters for API call ..."
+	#Set parameters
+	$parameters = @{
+		path = $Path 
 	}
 			
 	$parameters = $parameters | ConvertTo-Json
